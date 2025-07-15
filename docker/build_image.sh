120cb6b5758598811cf2ca3f4481914d5777047b
Kang-Woo Lee <kwlee@etri.re.kr>
2025-07-15 13:40:05 +0900

    docker 이미지 생성할 때 최신 jar를 사용하도록 수정.

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,7 +2,7 @@
 
 docker image rmi kwlee0220/mdt-client
 
-cp ../build/libs/mdt-client-1.0.0-all.jar mdt-client-all.jar
+cp ../build/libs/mdt-client-1.1.2-all.jar mdt-client-all.jar
 
 docker build -t kwlee0220/mdt-client:latest .
 docker push kwlee0220/mdt-client:latest

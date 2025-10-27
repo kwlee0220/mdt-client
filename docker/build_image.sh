b6c095a71501066a23413b81b9729ff67a9445f3
Kang-Woo Lee <kwlee@etri.re.kr>
2025-10-27 13:22:31 +0900

    docker 이미지 생성 스크립트 추가.

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,8 +1,35 @@
 #! /bin/bash
 
-docker image rmi kwlee0220/mdt-client
+VERSION="latest"
+REGISTRY=""
 
-cp ../build/libs/mdt-client-1.1.2-all.jar mdt-client-all.jar
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --version)
+      VERSION="$2"
+      shift 2
+      ;;
+    --registry)
+      REGISTRY="$2"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
 
-docker build -t kwlee0220/mdt-client:latest .
-docker push kwlee0220/mdt-client:latest
+if [ -z "$REGISTRY" ]; then
+  echo "Error: --registry 옵션이 필요합니다."
+  echo "Usage: $0 --registry REGISTRY [--version VERSION]"
+  exit 1
+fi
+
+docker image rmi $REGISTRY/mdt-client:$VERSION
+
+cp ../build/libs/mdt-client-1.2-all.jar mdt-client-all.jar
+
+docker build -t $REGISTRY/mdt-client:$VERSION .
+docker push $REGISTRY/mdt-client:$VERSION
+
+rm mdt-client-all.jar

952ada7db970a56902d8786458ec6c0f3751c6dc
Kang-Woo Lee <kwlee@etri.re.kr>
2025-11-10 11:51:59 +0900

    MqttBrokerConfig 클래스의 필드 접근자를 추가하고 생성자를 제거하여 Lombok을 사용하도록 수정

--- src/main/java/mdt/client/support/MqttBrokerConfig.java
+++ src/main/java/mdt/client/support/MqttBrokerConfig.java
@@ -1,9 +1,10 @@
 package mdt.client.support;
 
-import javax.annotation.Nullable;
-
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import utils.func.FOption;
 
@@ -12,25 +13,18 @@ import utils.func.FOption;
  *
  * @author Kang-Woo Lee (ETRI)
  */
+@Getter @Setter
+@Accessors(prefix="m_")
 @JsonIncludeProperties({ "brokerUrl", "connectionTimeout", "keepAliveInterval" })
 public class MqttBrokerConfig {
 	private static final String DEFAULT_BROKER_URL = "tcp://localhost:1883";
-	public static final MqttBrokerConfig DEFAULT = new MqttBrokerConfig(null, null, null);
 	
-	private final String m_brokerUrl;
-	private final String m_connectionTimeout;	// null means using default value of MQTT library.
-	private final String m_keepAliveInterval;	// null means using default value of MQTT library.
-	
-	public MqttBrokerConfig(@Nullable @JsonProperty("brokerUrl") String brokerUrl,
-							@Nullable @JsonProperty("connectTimeout") String connectionTimeout,
-							@Nullable @JsonProperty("keepAliveInterval") String keepAliveInterval) {
-		m_brokerUrl = FOption.getOrElse(brokerUrl, DEFAULT_BROKER_URL);
-		m_connectionTimeout = connectionTimeout;
-		m_keepAliveInterval = keepAliveInterval;
-	}
+	private String m_brokerUrl;
+	private String m_connectionTimeout;	// null means using default value of MQTT library.
+	private String m_keepAliveInterval;	// null means using default value of MQTT library.
 	
 	public String getBrokerUrl() {
-		return m_brokerUrl;
+		return FOption.getOrElse(m_brokerUrl, DEFAULT_BROKER_URL);
 	}
 	
 	public String getConnectionTimeout() {
